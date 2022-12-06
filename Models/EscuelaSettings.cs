bb31b33bd39b330bebf3fe2d48073904c8d0b7e4
MelyRamirez03 <melyddlovato@gmail.com>
2022-12-05 22:35:57 -0600

    ApiMongo

---
+++ Models/EscuelaSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace ApiMongo.Models
+{
+    public class EscuelaSettings:IEscuelaSettings
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string Collection { get; set; }
+    }
+
+    public interface IEscuelaSettings
+    {
+        string Server { get; set; }
+        string Database { get; set; }
+        string Collection { get; set; }
+
+    }
+}
